ebb0c88c01edb7c4128aca8c6aa15122a86c9d50
Jonayed Al Habib <jonayedalhabibm135@gmail.com>
2026-09-06 21:12:32 +0600

    feat: add Skill component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero'
 import Work from '../components/Work';
+import Skill from '../components/Skill'
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
       <Work />
+      <Skill />
     </div>
   )
 }
