d2ce77c11353355ac22a3bcb7bcbf5d76f034ba3
Jonayed Al Habib <jonayedalhabibm135@gmail.com>
2026-09-06 16:29:43 +0600

    feat: add Work component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero'
+import Work from '../components/Work';
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Work />
     </div>
   )
 }
